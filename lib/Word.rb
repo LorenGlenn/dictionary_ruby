6f92c83f6a9a807f4b18fede05cc16740b85f43e
Loren Glenn <lorencglenn@gmail.com>
2017-01-12 16:22:42 -0800

    finish creating word object

---
+++ lib/Word.rb
@@ -0,0 +1,36 @@
+class Word
+  @@words = []
+  attr_reader(:word)
+
+  define_method(:initialize) do |attributes|
+    @word = attributes.fetch(:input)
+    @id = @@words.length().+(1)
+    @definitions = []
+end
+
+define_singleton_method(:all) do
+  @@words
+end
+
+define_method(:save) do
+  @@words.push(self)
+end
+
+define_singleton_method(:clear) do
+  @@words = []
+end
+
+define_method(:add_definition) do |definition|
+  @definitions.push(definition)
+end
+
+define_singleton_method(:find) do |id|
+    found_word = nil
+    @@words.each() do |word|
+      if word.id().eql?(id)
+        found_word = word
+      end
+    end
+    found_word
+  end
+end
