0c83aba3eb92bcc1c2d5b69ae31015630a7896fe
Loren Glenn <lorencglenn@gmail.com>
2017-01-12 17:52:49 -0800

    small refactoring

--- app.rb
+++ app.rb
@@ -1,51 +1,32 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
-require('./lib/vehicle')
-require('./lib/dealership')
+require('./lib/word')
+require('./lib/definition')
 
 get('/') do
+  @words = Word.all()
   erb(:index)
 end
 
-get('/dealerships/new') do
-   erb(:dealerships_form)
+post('/') do
+  newWord = params.fetch('word')
+  Word.new({:word => newWord}).save()
+  @words = Word.all()
+  erb(:index)
 end
 
-get('/dealerships') do
-  @dealerships = Dealership.all()
-  erb(:dealerships)
+get('/word/:id') do
+  @word = Word.find(params.fetch('id').to_i())
+  @definitions = @word.definitions()
+  erb(:word)
 end
 
-post('/dealerships') do
-  name = params.fetch('name')
-  Dealership.new(name).save()
-  @dealerships = Dealership.all()
-  erb(:success)
-end
-
-get('/vehicles/:id') do
-  @vehicle = Vehicle.find(params.fetch('id').to_i())
-  erb(:vehicle)
-end
-
-get('/dealerships/:id') do
-  @dealership = Dealership.find(params.fetch('id').to_i())
-  erb(:dealership)
-end
-
-get('/dealerships/:id/vehicles/new') do
-    @dealership = Dealership.find(params.fetch('id').to_i())
-    erb(:dealership_vehicles_form)
-end
-
-post('/vehicles') do
-  make = params.fetch('make')
-  model = params.fetch('model')
-  year = params.fetch('year')
-  @vehicle = Vehicle.new(make, model, year)
-  @vehicle.save()
-  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
-  @dealership.add_vehicle(@vehicle)
-  erb(:success)
+post('/word/:id') do
+  newDefinition = params.fetch('definition')
+  addedDefinition = Definition.new({:definition => newDefinition}).save()
+  @word = Word.find(params.fetch('word_id').to_i())
+  @word.add_definition(addedDefinition)
+  @definitons = @word.definitions()
+  erb(:word)
 end
