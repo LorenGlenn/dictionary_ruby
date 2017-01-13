da07a9017d1418526017ca5967ae54c4f3eef7fd
Loren Glenn <lorencglenn@gmail.com>
2017-01-12 16:28:17 -0800

    complete definition object

---
+++ lib/Definition.rb
@@ -0,0 +1,30 @@
+class Definition
+  @@allDefinitions = []
+
+  define_method(:initialize) do |attributes|
+    @definition = attributes.fetch(:definition)
+    @id = @@allDefinitions.length().+(1)
+  end
+
+  define_singleton_method(:all) do
+    @@allDefinitions
+  end
+
+  define_singleton_method(:clear) do
+    @@allDefinitions = []
+  end
+
+  define_method(:save) do
+    @@allDefinitions.push(self)
+  end
+
+  define_singleton_method(:find) do |id|
+    found_definition = nil
+    @@allDefinitions.each() do |define|
+      if define.id().eql?(id)
+        found_definition = define
+      end
+    end
+    found_definition
+  end
+end
